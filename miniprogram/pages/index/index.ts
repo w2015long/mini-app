26714fe197e8c36b38340fa681f1891c2565e175
Jsen <w2015long@qq.com>
2020-08-09 17:07:31 +0800

    feat: swipre

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,57 +1,26 @@
 //index.js
 //获取应用实例
-import { IMyApp } from '../../app'
-
-const app = getApp<IMyApp>()
+// import { IMyApp } from '../../app';
+import { request } from '../../utils/request.ts';
+// const app = getApp<IMyApp>();
 
 Page({
-  data: {
-    motto: '点击 “编译” 以构建',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-  },
-  //事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad() {
-    if (app.globalData.userInfo) {
-      this.setData!({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true,
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = (res) => {
-        this.setData!({
-          userInfo: res,
-          hasUserInfo: true
+    data: {
+        // 轮播图数组
+        swiperList: [],
+    },
+    //事件处理函数
+    onLoad() {
+        this.getSwiperList();
+        console.log(999);
+    },
+    // 获取轮播图数据
+    async getSwiperList(){
+        // /home/swiperdata
+        const result = await request({ url: '/home/swiperdata' });
+        console.log(result);
+        this.setData({
+            swiperList: result,
         })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData!({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-
-  getUserInfo(e: any) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData!({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+    },
 })
