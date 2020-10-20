59f3ec5a16b74a32575afb4dc2f98300eb090943
Jason <45002790+w2015long@users.noreply.github.com>
2020-10-20 15:22:34 +0800

    Update cart.js

--- miniprogram/pages/cart/cart.js
+++ miniprogram/pages/cart/cart.js
@@ -6,27 +6,6 @@ Page({
    */
   data: {
     chatList: [
-      {
-        id: 0, 
-        avatar: '../../icons/avatar-jason.jpg',
-        name: 'JasonWang',
-        message: '[图片]',
-        date: '18:32'
-      },
-      {
-        id: 1, 
-        avatar: 'https://st-cn.meishij.net/user/69/207/ns2801819_147937132568875.jpg',
-        name: '科室-护',
-        message: '好的',
-        date: '14:06'
-      },
-      {
-        id: 2, 
-        avatar: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3526287864,2288992690&fm=26&gp=0.jpg',
-        name: '三院-张',
-        message: '一会到家',
-        date: '16:12'
-      },
     ],
   },
   goChat (e) {
